8de961c76e20af7a9b0eec40e121dc56827d976f
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-01-22 22:35:01 +0100

    Add files via upload

---
+++ TurnirAistentModel/AsistentGUI/PregledTurnira.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsistentGUI
+{
+    public partial class PregledTurnira : Form
+    {
+        public PregledTurnira()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
